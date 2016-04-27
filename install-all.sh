44f4ef01378725430c9a782f75930b086b565d68
Will Price <will.price94@gmail.com>
2016-04-27 16:09:34 +0100

    install-all.sh will now link to $PWD

--- install-all.sh
+++ install-all.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # vim: set ft=sh:
+target_directory="$PWD"
+cd $(dirname $(realpath $0))
 
 EXCLUDED_PACKAGES_REGEX=".*\(system\|personal\|x230\).*"
 
 for package in $(find . -mindepth 1 -maxdepth 1 -type d ! -regex "$EXCLUDED_PACKAGES_REGEX" ! -name '.*'); do
-  stow --restow "${package##./}"
+  stow --restow --target "$target_directory" "${package##./}"
 done
 
 echo "The following packages haven't been linked as you may or may not want them on all machines"
