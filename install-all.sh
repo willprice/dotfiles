346f4263d8617f11036e40ebeee4e0b06b7aea3d
Will Price <will.price94@gmail.com>
2016-08-15 23:33:48 +0100

    Refactor: install, split out install script

--- install-all.sh
+++ install-all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # vim: set ft=sh:
-target_directory="${1:-$PWD}"
+target_directory="${1:-$HOME}"
 cd $(dirname $(realpath $0))
 
 EXCLUDED_PACKAGES_REGEX=".*\(system\|personal\|x230\).*"
 
 for package in $(find . -mindepth 1 -maxdepth 1 -type d ! -regex "$EXCLUDED_PACKAGES_REGEX" ! -name '.*'); do
-  stow --restow --target "$target_directory" "${package##./}"
+  ./install.sh "$target_directory" "${package##./}"
 done
 
 echo "The following packages haven't been linked as you may or may not want them on all machines"
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo "./install.sh <TARGET PATH> <PACKAGE NAME>"
+    echo "./install.sh $HOME vim"
+}
+
+[[ $# -ne 2 ]] && { usage; exit 1; }
+
+target_directory="$1"; shift
+package="$1"; shift
+
+cd $(dirname $(realpath $0))
+stow --restow --target "$target_directory" "$package"
+
+# vim: set ft=sh:
