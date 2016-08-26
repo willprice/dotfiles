e29647b499436e89b06f95d0ab5fb2e3e7ae5210
Will Price <will.price94@gmail.com>
2016-08-26 23:05:19 +0100

    Cleanup: Remove unused variables from fish env

--- fish/.config/omf/custom/variables.fish
+++ fish/.config/omf/custom/variables.fish
@@ -7,6 +7,4 @@ set --export VISUAL        "$editor"
 set --export BROWSER       chromium
 set --export TERMINAL      urxvt
 set --export GOPATH        "$HOME/code/go"
-set --export ANSIBLE_HOME  ~/src/ansible
-set --global Z_SCRIPT_PATH ~/src/z/z.sh
 set --global PYTHONDOCS    /usr/share/doc/python2/html/
