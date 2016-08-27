1c50dd0bcee18af563050139a97870ff1206739b
Will Price <will.price94@gmail.com>
2016-08-27 20:54:10 +0100

    Bugfix: omf, correct jre bin PATH append

--- fish/.config/omf/custom/jre.fish
+++ fish/.config/omf/custom/jre.fish
@@ -1,4 +1,4 @@
 # Do not change this unless you want to completely by-pass Arch Linux' way
 # of handling Java versions and vendors. Instead, please use script `archlinux-java`
-set -x PATH "$PATH:/usr/lib/jvm/default/bin"
+set -x PATH $PATH /usr/lib/jvm/default/bin
 set -x JAVA_HOME /usr/lib/jvm/default
