96edcd551c86b969942a3cefca66fd2d501e09d6
Will Price <will.price94@gmail.com>
2016-09-21 21:34:01 +0100

    Make `p` pacman alias smart

--- fish/.config/omf/aliases/pacman.fish
+++ fish/.config/omf/aliases/pacman.fish
@@ -1,3 +1,25 @@
 alias pacman "sudo pacman"
-alias p "pacman"
 alias psyu "pacman -Syu"
+
+function p
+    set has_options 0
+
+    if [ (count $argv) -eq 0 ]
+        sudo pacman -Syu
+        return
+    end
+
+    for arg in $argv
+        if echo $arg | grep -E '^-' 2>&1 > /dev/null
+            set has_options 1
+        end
+    end
+
+    if [ $has_options -eq 1 ]
+        sudo pacman $argv
+    else
+        sudo pacman -S $argv
+    end
+end
+
+alias yrt "yaourt -S --noconfirm"
