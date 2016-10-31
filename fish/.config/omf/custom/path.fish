c98c00905ada71d075cac4b3c5c9e6fee60ac2f8
Will Price <will.price94@gmail.com>
2016-10-31 11:21:05 +0000

    [fish] Use add_to_path_if_exists when setting up PATH
    Previously we were just adding the directories even if they didn't
    exist, they're now checked which prevents adding the directories to the
    path if the don't exist

--- fish/.config/omf/custom/path.fish
+++ fish/.config/omf/custom/path.fish
@@ -1,9 +1,12 @@
-set --export PATH     "$HOME/.cabal/bin" \
-                      "$HOME/code/go/bin" \
-                      "$HOME/.bin"\
-                      "$HOME/.gem/ruby/"*/bin \
-                      "$HOME/.rvm/bin" \
-                      $PATH
-
+set -l candidate_path_directories \
+      "$HOME/.cabal/bin" \
+      "$HOME/code/go/bin" \
+      "$HOME/.bin"\
+      "$HOME/.gem/ruby/"*/bin \
+      "$HOME/.rvm/bin" \
+      "$HOME/.local/bin"
+for dir in $candidate_path_directories
+    add_to_path_if_exists "$dir"
+end
 
 # vim: set ft=fish:
