650639f8ff694b888bb66c7aa6d91cb62506cc0d
Will Price <will.price94@gmail.com>
2018-06-28 14:01:33 +0100

    [bash] Dont conditionally set aliases

--- bash/.bashrc.d/functions/alias_command.sh
+++
@@ -1,8 +0,0 @@
-function alias_command {
-    local cmd="$1"; shift
-    local replacement="$1"; shift
-
-    if command -v "$cmd" >/dev/null 2>&1; then
-        alias "${cmd}=${replacement}";
-    fi
-}
