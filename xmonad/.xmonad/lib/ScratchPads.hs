621d9f76e9e7c18f56de120ee565441a7b20de6f
Will Price <will.price94@gmail.com>
2018-10-28 21:03:25 +0000

    [xmonad] Show zeal on 2/3 of screen

--- xmonad/.xmonad/lib/ScratchPads.hs
+++ xmonad/.xmonad/lib/ScratchPads.hs
@@ -2,6 +2,7 @@ module ScratchPads (scratchPads) where
 
 import XMonad
 import XMonad.Util.NamedScratchpad
+import qualified XMonad.StackSet as W
 
 scratchPads :: [NamedScratchpad]
-scratchPads = [NS "zeal" "zeal" (className =? "Zeal") nonFloating]
+scratchPads = [NS "zeal" "zeal" (className =? "Zeal") (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))]
