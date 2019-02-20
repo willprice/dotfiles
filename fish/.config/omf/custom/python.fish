fafba4e5f482b8b9f5f24b9687684df6571eb7fe
Will Price <will.price94@gmail.com>
2019-02-20 19:28:15 +0000

    [fish] Only import virtualfish if it is installed

--- fish/.config/omf/custom/python.fish
+++ fish/.config/omf/custom/python.fish
@@ -6,6 +6,10 @@ alias ipy=ipython
 alias ipy3=ipython3
 alias ipy2=ipython2
 
+if python -c 'import virtualfish'
+    eval (python -m virtualfish auto_activation)
+end
+
 if test -d ~/.pyenv
     set -x PYENV_ROOT "$HOME/.pyenv"
     set -x PATH "$PYENV_ROOT/bin" $PATH
