c47850f1e28112cc433404bd43b687ebee9aa831
Will Price <will.price94@gmail.com>
2021-02-25 08:26:04 +0000

    fish: Remove virtualfish, dont use it anymore

--- fish/.config/omf/custom/python.fish
+++ fish/.config/omf/custom/python.fish
@@ -6,10 +6,6 @@ alias ipy=ipython
 alias ipy3=ipython3
 alias ipy2=ipython2
 
-if python3 -c 'import virtualfish'
-    eval (python3 -m virtualfish auto_activation)
-end
-
 if test -d ~/.pyenv
     set -x PYENV_ROOT "$HOME/.pyenv"
     set -x PATH "$PYENV_ROOT/bin" $PATH
