00a674999aa8d5ce8ec2bf834c3ede1740aa2fdd
Will Price <will.price94@gmail.com>
2024-11-03 19:38:19 +0000

    Remove xmonad, hyprland is my replacement

--- taffybar/.config/taffybar/src/TaffyColor.hs
+++
@@ -1,5 +0,0 @@
-module TaffyColor where
-
-class TaffyColor c where
-        toTaffyColor :: c -> (Double, Double, Double, Double) -- (0-1, 0-1, 0-1, 0-1) RGBA tuple
-
--- xmonad/.xmonad/lib/Colours.hs
+++
@@ -1,6 +0,0 @@
-module Colours where
-
-lightBlue = "#ABF4FC"
-lightPink = "#FF1978"
-darkGrey  = "#1A1A1A"
-steelBlue = "#6CA2AD"
